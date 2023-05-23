8fc1f3176046ffbac314ceea4608ff84ebf27698
Teleфonчиk <matvey-sizov@mail.ru>
2023-05-23 13:10:37 +0300

    delete: built-in regexps

--- pkg/bind/helpers.go
+++ pkg/bind/helpers.go
@@ -13,6 +13,7 @@ var (
 	LinkRegexp  = regexp.MustCompile(`^https?://(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b[-a-zA-Z0-9()@:%_+.~#?&/=]*$`)
 	TitleRegexp = regexp.MustCompile(`^\p{L}+(?:([ \-']|(\. ))\p{L}+)*$`)
 	EmailRegexp = regexp.MustCompile(`^\S+@\S+\.\S+$`)
+	UUID4       = regexp.MustCompile(`^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$`)
 )
 
 // FillStruct of given generic type by request JSON body
@@ -38,8 +39,4 @@ func validateLink(fl validator.FieldLevel) bool {
 
 func validateTitle(fl validator.FieldLevel) bool {
 	return TitleRegexp.MatchString(fl.Field().String())
-}
-
-func validateEmail(fl validator.FieldLevel) bool {
-	return EmailRegexp.MatchString(fl.Field().String())
 }
